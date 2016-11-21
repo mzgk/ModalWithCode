d03a48c9c9524f7752e64dd2c7dd8df36ed06568
mzgk <mzgkworks@gmail.com>
2016-11-21 23:23:14 +0900

    ADD : ModalVCにデータを渡す。ModalVCを閉じる。ModalVCからデータを受け取る。

--- ModalWithCode/ModalViewController.swift
+++ ModalWithCode/ModalViewController.swift
@@ -8,12 +8,27 @@
 
 import UIKit
 
+// 自身を閉じるためのDelegateプロトコル
+protocol ModalViewControllerDelegate {
+    // inheritText : Delegate先に引き継ぐ値
+    func modalViewController(sender: ModalViewController, inheritText: String)
+}
+
+
 class ModalViewController: UIViewController {
+
+    // delegate先を格納する変数
+    var delegate: ModalViewControllerDelegate?
+    // 遷移元からの引き継ぎデータ
+    var inheritData = ""
+    @IBOutlet weak var modalLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        // ここでUIにセットする
+        modalLabel.text = inheritData
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +36,11 @@ class ModalViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+
+// MARK: - ボタンタップ時にdelegate（ViewController）に実装したDelegateメソッドを実行する
+    @IBAction func tapCloseButton(_ sender: UIButton) {
+        self.delegate?.modalViewController(sender: self, inheritText: "Closed")
+    }
 
     /*
     // MARK: - Navigation
--- ModalWithCode/ViewController.swift
+++ ModalWithCode/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// 表示したModalViewを閉じる責務があるので、ModalViewControllerDelegateを宣言
+class ViewController: UIViewController, ModalViewControllerDelegate {
 
+    @IBOutlet weak var closedLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,13 +23,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+
+// MARK: - ModalViewを表示する
     @IBAction func tapModalButton(_ sender: UIButton) {
         let modal = self.storyboard?.instantiateViewController(withIdentifier: "ModalSB") as! ModalViewController
         modal.modalTransitionStyle = .coverVertical
         modal.modalPresentationStyle = .pageSheet
+        modal.delegate = self
 
+        // データを渡す（UIはまだ生成されていないので、直接UIにセットはできない）
+        modal.inheritData = "Show"
+
+        // Modalで表示する
         self.present(modal, animated: true, completion: nil)
     }
 
+    
+// MARK: - ModalViewを閉じる
+    // ModalViewControllerDelegate
+    func modalViewController(sender: ModalViewController, inheritText: String) {
+        // 表示したModalを閉じる
+        self.dismiss(animated: true, completion: nil)
+
+        // Modal側から渡されたデータをUIにセットする
+        closedLabel.text = inheritText
+    }
 }
 
